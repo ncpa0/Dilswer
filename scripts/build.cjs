6bcbb430d55a63157a9d02d439335c667abff6e7
ncpa0cpl <szymonb21@gmail.com>
2022-09-12 19:29:06 +0200

    build: updated the build script to exit with status 1 on error

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -1,18 +1,27 @@
 const path = require("path");
 const build = require("@ncpa0cpl/nodepack").build;
 
-build({
-  target: "es6",
-  srcDir: path.resolve(__dirname, "../src"),
-  outDir: path.resolve(__dirname, "../dist"),
-  formats: ["cjs", "esm", "legacy"],
-  tsConfig: path.resolve(__dirname, "../tsconfig.build.json"),
-  declarations: true,
-  pathAliases: {
-    "@DataTypes/*": "./data-types/*",
-    "@Utilities/*": "./utilities/*",
-    "@Validation/*": "./validation-algorithms/*",
-    "@Intrinsic/*": "./intrinsic-type-utils/*",
-    "@JSONSchemaParser/*": "./json-schema-parser/*",
-  },
-});
+async function main() {
+  try {
+    await build({
+      target: "es6",
+      srcDir: path.resolve(__dirname, "../src"),
+      outDir: path.resolve(__dirname, "../dist"),
+      formats: ["cjs", "esm", "legacy"],
+      tsConfig: path.resolve(__dirname, "../tsconfig.build.json"),
+      declarations: true,
+      pathAliases: {
+        "@DataTypes/*": "./data-types/*",
+        "@Utilities/*": "./utilities/*",
+        "@Validation/*": "./validation-algorithms/*",
+        "@Intrinsic/*": "./intrinsic-type-utils/*",
+        "@JSONSchemaParser/*": "./json-schema-parser/*",
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+main();
