c38e3c92abac27310213ef02b475643acedb2ab6
ncpa0cpl <szymonb21@gmail.com>
2023-03-08 18:31:11 +0100

    fix: false-positive results in some rare cases with circular types

--- src/validation-algorithms/validators/helper-validated-circ-values.ts
+++ src/validation-algorithms/validators/helper-validated-circ-values.ts
@@ -1,3 +1,5 @@
+import type { AnyDataType } from "@DataTypes/types";
+
 /**
  * Collection of values that have been ran through the validator
  * already, and were expected to possibly contain circular
@@ -10,4 +12,21 @@
  *
  * This Set should be cleared after each validation pass.
  */
-export const validatedCircularValues = new Set();
+export const validatedCircularValues = new Map<AnyDataType, Set<any>>();
+
+export const wasCircValidated = (type: AnyDataType, data: unknown) => {
+  let set = validatedCircularValues.get(type);
+
+  if (!set) {
+    set = new Set([data]);
+    validatedCircularValues.set(type, set);
+    return false;
+  }
+
+  if (set.has(data)) {
+    return true;
+  }
+
+  set.add(data);
+  return false;
+};
--- src/validation-algorithms/validators/validate-circular.ts
+++ src/validation-algorithms/validators/validate-circular.ts
@@ -1,14 +1,13 @@
 import type { Circular, CircularRef } from "@DataTypes/data-types";
 import type { AnyDataType } from "@DataTypes/types";
 import type { Path } from "@Validation/path";
-import { validatedCircularValues } from "@Validation/validators/helper-validated-circ-values";
+import { wasCircValidated } from "@Validation/validators/helper-validated-circ-values";
 import { validatorsLookupMap } from "@Validation/validators/validate-type";
 
 export const validateCircular = (path: Path, type: Circular, data: unknown) => {
-  if (validatedCircularValues.has(data)) {
+  if (wasCircValidated(type, data)) {
     return;
   }
-  validatedCircularValues.add(data);
 
   const childType = type.type as AnyDataType;
   return validatorsLookupMap.get(childType.kind)!(path, childType, data);
@@ -19,10 +18,9 @@ export const validateCircularRef = (
   type: CircularRef,
   data: unknown
 ) => {
-  if (validatedCircularValues.has(data)) {
+  if (wasCircValidated(type._getReferencedType(), data)) {
     return;
   }
-  validatedCircularValues.add(data);
 
   const refType = type._getReferencedType() as AnyDataType;
   return validatorsLookupMap.get(refType.kind)!(path, refType, data);
